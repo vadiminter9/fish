ce2ecfe38af084e9a0a136d8e81a1533ce4fb4ea
Vadim Ohapkin (Akvelon INC) <v-vaohap@microsoft.com>
2019-11-29 17:32:18 +0300

    Fish behaviour

--- Fish/Assets/Scripts/FishBehaviour.cs
+++ Fish/Assets/Scripts/FishBehaviour.cs
@@ -9,13 +9,18 @@ public class FishBehaviour : MonoBehaviour
     System.Random random = new System.Random();
     public float speed = 1;
     private const float destinationDelata = 3f;
+    private const float angleDelta = 6f;
 
     private bool firstStart = true;
     private Vector2 nextDestination;
 
     void Start()
     {
+        transform.position = GetNewDestination();
         nextDestination = GetNewDestination();
+
+        //transform.position = new Vector2(8, 3);
+        //nextDestination = new Vector2(-8, 3);
     }
 
     void Update()
@@ -40,7 +45,23 @@ public class FishBehaviour : MonoBehaviour
 
         var yMove = speed * (float)Math.Sin(alpha) * Time.deltaTime;
         position.y = position.y + yMove;
+
         transform.position = position;
+
+        Vector3 target = new Vector3(nextDestination.x, nextDestination.y, 0);
+
+        // Angular speed in radians per sec.
+        float rotationSpeed = 1.0f;
+
+
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = (target - transform.position).normalized;
+
+        /*transform.rotation*/
+        var lookRotation = Quaternion.LookRotation(targetDirection);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+
     }
 
     private static Vector2 GetNewDestination()
--- Fish/Assets/Scripts/GameController.cs
+++ Fish/Assets/Scripts/GameController.cs
@@ -8,7 +8,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i=0; i<15; i++)
+        for(int i=0; i<8; i++)
 		{
 			Instantiate(fish[i]);
 		}
