cc9bea14a7df25076ecb38a29d403ce8eba98c92
Vladimir Trunov <iv.vovatrunov__37@mail.ru>
2019-11-29 17:03:47 +0300

    Added size and count.

--- Fish/Assets/Scripts/GameController.cs
+++ Fish/Assets/Scripts/GameController.cs
@@ -8,15 +8,90 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i=0; i<15; i++)
-		{
-			Instantiate(fish[i]);
-		}
+        foreach (var f in fish)
+        {
+            f.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+
+        List<Group> groups = new List<Group>
+        {
+            new Group("Group 1", 15),
+            new Group("Group 2", 2),
+            new Group("Group 3", 7)
+        };
+
+        this.GenerateFish(groups, Type.Size);
     }
 
-    /*// Update is called once per frame
-    void Update()
+    void GenerateFish(List<Group> groups, Type type)
     {
-        
-    }*/
+        if (type == Type.Amount)
+        {
+            this.AddFishByAmount(groups);
+        }
+        else
+        {
+            this.AddFishBySize(groups);
+        }
+    }
+
+    private void AddFishByAmount(List<Group> groups)
+    {
+        for (int i = 0; i < groups.Count; i++)
+        {
+            for (int j = 0; j < groups[i].Value; j++)
+            {
+                Instantiate(fish[i]);
+            }
+        }
+    }
+
+    private void AddFishBySize(List<Group> groups)
+    {
+        double totalSum = this.GetTotalValue(groups);
+
+        float difference = 0.0125f;
+
+        for (int i = 0; i < groups.Count; i++)
+        {
+            float toIncrease = (float)(0.25 + difference * GetPercent(groups[i].Value, totalSum));
+            fish[i].transform.localScale = new Vector3(toIncrease, toIncrease, toIncrease);
+            Instantiate(fish[i]);
+        }
+    }
+
+    private double GetTotalValue(List<Group> groups)
+    {
+        double sum = 0;
+        foreach(Group group in groups)
+        {
+            sum += group.Value;
+        }
+
+        return sum;
+    }
+
+    private double GetPercent(double value, double totalvalue)
+    {
+        return value / totalvalue * 100;
+    }
+}
+
+public class Group
+{
+    public Group(string name, int amount)
+    {
+        this.Name = name;
+        this.Value = amount;
+    }
+
+    public string Name { get; set; }
+
+    public double Value { get; set; }
+}
+
+public enum Type
+{
+    Amount,
+    Size
 }
