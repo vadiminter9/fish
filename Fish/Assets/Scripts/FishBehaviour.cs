826aff389e567dca99d3229266376163e354c5d5
Vadim Ohapkin (Akvelon INC) <v-vaohap@microsoft.com>
2019-11-29 10:51:31 +0300

    Added fishes

---
+++ Fish/Assets/Scripts/FishBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FishBehaviour : MonoBehaviour
+{
+    System.Random random = new System.Random();
+    public float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*float horizontal = Random.Next();
+		float vertical = Random.Next();
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");*/
+
+        Vector2 position = transform.position;
+        position.x = position.x + speed * (float)random.NextDouble() * Time.deltaTime;
+        position.y = position.y + speed * (float)random.NextDouble() * Time.deltaTime;
+        transform.position = position;
+    }
+}
---
+++ Fish/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject fish;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<4; i++)
+		{
+			Instantiate(fish);
+		}
+    }
+
+    /*// Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
